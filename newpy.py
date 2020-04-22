fcd72b12708726545245aa81a76b21d359b09d8c
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-22 18:17:42 +0530

    test

--- newpy.py
+++ newpy.py
@@ -1,4 +1,5 @@
 import boto3
+rekognition = boto3.client("rekognition")
 translate = boto3.client("translate")
 
 def handler(event, context):
@@ -12,5 +13,19 @@ def handler(event, context):
     except BaseException as e:
         print(e)
         raise(e)
+        try:
+            data = rekognition.detect_text(
+                Image={
+                    'S3Object': {
+                        'Bucket': "cloud9-ktest",
+                        'Name': "shot-buffalo-5daeeb9351a540214a19d115-wiu",
+                        'Version': "png"
+                    }
+                }
+            )
+            print(data)
+        except BaseException as e:
+            print(e)
+            raise(e)
     
     return {"message": "Successfully executed"}
