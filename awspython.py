898d200f772023ba291b85e380328b10c4ed96eb
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-22 17:32:46 +0530

    test

--- awspython.py
+++ awspython.py
@@ -1,3 +1,5 @@
+import boto3
+translate = boto3.client("translate")
 import requests
 
 def handler(event, context):
@@ -8,9 +10,20 @@ def handler(event, context):
             headers={"Accept":"application/json"}
         )
         # your code goes here
+        print(res)
     except BaseException as e:
         # error handling goes here
         print(e)
         raise(e)
+        try:
+            data = translate.translate_text(
+                SourceLanguageCode="auto",
+                TargetLanguageCode="en",
+                Text="Hola"
+            )
+            print(data)
+        except BaseException as e:
+            print(e)
+            raise(e)
     
     return {"message": "Successfully executed"}
